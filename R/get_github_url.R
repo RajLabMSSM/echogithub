e3071ae771db2c7b24e4b076634cfa2776f1d2c5
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-12-23 21:56:02 -0500

    add get_github_url

--- R/get_github_url.R
+++ R/get_github_url.R
@@ -1,9 +1,16 @@
 get_github_url <- function(desc_file){
-    if(length(grep("github",desc_file$URL))>0){
-        return(desc_file$URL)
+    ## These fields sometimes contain >1 link.
+    ## Find the one that's actually for the GitHub repo.
+    get_gh <- function(URL){
+        urls <- trimws(strsplit(URL,",")[[1]])
+        grep("https://github.com", urls, value = TRUE)
+    }
+    #### Parse ####
+    if(length(grep("github",desc_file$URL))>0){ 
+        return(get_gh(desc_file$URL))
     } else if (length(grep("github",desc_file$BugReports))>0){
         return(
-            trimws(gsub("issues$","",desc_file$BugReports),
+            trimws(gsub("issues$","",get_gh(desc_file$BugReports)),
                    whitespace = "/")
         )
     } else if (!is.null(desc_file$git_url)){
